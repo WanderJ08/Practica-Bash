7d5242ffca348fe8a40f66e1b685a36a77f7118e
Wander Jerez <Wanderroque01@gmail.com>
2023-02-25 17:44:45 -0400

    modifique el nombre del archivo teoria bash

--- Teoria_Bash.sh
+++ Teoria_Bash.sh
@@ -1,18 +1,2 @@
 
 #!/usr/bin/env bash
-#Crear un programa en el que pueda ir digitando numeros hasta que encuentre el numero que tengo establecido
-
-Meta=7;
-echo "Digite un numero: "
-read opcion
-
-while [ "$opcion" != "$Meta" ]
-do
-    clear
-    echo "Este numero no es el indicado"
-    echo "Digite otro numero: "
-    read opcion
-done
-clear
-echo "Felicidadeeeessss"
-echo "Haz elegido el numero adecuado"
